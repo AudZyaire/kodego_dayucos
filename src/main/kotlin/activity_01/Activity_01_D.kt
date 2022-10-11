9846466b5072013e40bc11f537e0a628c7a31397
AudZyaire <jjdayucos04@gmail.com>
2022-10-11 19:37:46 +0800

    removed class Main

--- src/main/kotlin/activity_01/Activity_01_D.kt
+++ src/main/kotlin/activity_01/Activity_01_D.kt
@@ -1,5 +1,3 @@
-import kotlin.system.exitProcess
-
 /* Create an application that will accept 5 monetary amounts.
 After the input is done, the user will be asked “Divide the value by how many?”. It will only accept an Integer as input.
 The total of the 5  input will be divided by the answer in the second question input.
