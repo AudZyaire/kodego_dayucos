19438544471a52b38d8e12429da0913908cfe45d
AudZyaire <jjdayucos04@gmail.com>
2022-10-21 20:21:46 +0800

    Enhanced Activity_01_F code

--- src/main/kotlin/Activity_01_F/UniqueCharacters.kt
+++ src/main/kotlin/Activity_01_F/UniqueCharacters.kt
@@ -5,6 +5,7 @@
  */
 
 package Activity_01_F
+
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger{}
@@ -13,34 +14,82 @@ fun main() {
 
     var string1: String = ""
     var string2: String = ""
-    var index: Int = 0
-
-    //declare Unique characters variable as SET to combine duplicate values
-    var string1Unique:HashSet<Char> = hashSetOf()
-    var string2Unique:HashSet<Char> = hashSetOf()
+    var string1Unique: String = ""
+    var string2Unique: String = ""
+    var string1NoDupe: String = ""
+    var string2NoDupe: String = ""
+    var uniqueCombined: String = ""
+    var uniqueCombinedFormat: String = ""
+    var counter = 0
 
     logger.info { "Enter first String: " }
     string1 = readln()
     logger.info { "Enter second String: " }
     string2 = readln()
 
-    do {
-        if (string1[index].uppercase() !in string2.uppercase()) {
-            string1Unique.add(string1[index])
+//compare string1 elements to itself, store to string1Unique if no duplicate
+    for (characters in string1) {
+        for (characterCompare in string1) {
+            if (characters.uppercase() == characterCompare.uppercase()) {
+                counter++
+            }
         }
-        index++
-    }while(index < string1.length)
-
-    index = 0 //assign index to 0 to be used for another loop
-
-    do {
-        if (string2[index].uppercase() !in string1.uppercase()) {
-            string2Unique.add(string2[index])
+        if (counter < 2) {
+            // println("$characters is unique")
+            string1Unique += characters
         }
-        index++
-    }while(index < string2.length)
+        counter = 0
+    }
+counter = 0
+    //compare string2 elements to itself, store to string2Unique if no duplicate
+    for (characters in string2) {
+        for (characterCompare in string2) {
+            if (characters.uppercase() == characterCompare.uppercase()) {
+                counter++
+            }
+        }
+        if (counter < 2) {
+            // println("$characters is unique")
+            string2Unique += characters
+        }
+        counter = 0
+    }
+    counter = 0
 
-    logger.info { "Unique elements in the first string: $string1Unique" }
-    logger.info { "Unique elements in the second string: $string2Unique" }
+    //compare string1Unique characters to string2
+    for (characters in string1Unique) {
+        for (char in string2) {
+            if (characters.uppercase() == char.uppercase()) {
+                counter++
+            }
+        }
+        if (counter == 0) {
+            string1NoDupe += characters
+        }
+        counter = 0
+    }
+    counter = 0
 
+    //comparing string2Unique characters to string1
+    for (characters in string2Unique) {
+        for (char in string1) {
+            if (characters.uppercase() == char.uppercase()) {
+                counter++
+            }
+        }
+        if (counter == 0) {
+            string2NoDupe += characters
+        }
+        counter = 0
+
+    }
+        logger.info { "Removing duplicates..." }
+        logger.info { "Unique elements in the first string: $string1NoDupe" }
+        logger.info { "Unique elements in the second string: $string2NoDupe" }
+    uniqueCombined = string1NoDupe + string2NoDupe
+    for (i in uniqueCombined) {
+        uniqueCombinedFormat += "[ $i ]"
+    }
+    logger.info { "There are ${uniqueCombined.length} unique characters in both strings" }
+    logger.info { "Unique from both strings are: $uniqueCombinedFormat" }
 }
