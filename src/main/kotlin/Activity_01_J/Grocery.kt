c3aa402fc7018745b09ff18742fbca74e3efd357
AudZyaire <jjdayucos04@gmail.com>
2022-10-25 14:52:52 +0800

    Modified Activity_01_J - Grocery.kt.kt - added error handling and GUI

--- src/main/kotlin/Activity_01_J/Grocery.kt
+++ src/main/kotlin/Activity_01_J/Grocery.kt
@@ -1,7 +1,6 @@
 package Activity_01_J
 import mu.KotlinLogging
 private val logger = KotlinLogging.logger{}
-
 fun main() {
     var numberList: ArrayList<Int> = arrayListOf()
     var itemsBoughtList: ArrayList<String> = arrayListOf()
@@ -14,55 +13,89 @@ fun main() {
     var quantity: String
     var price: String = ""
     var yesNo: String = "Y"
+    var guiAnswer: String? = null
+    var guiAnswerTypeCast: Int
+    var programExit:Boolean = false
 
-    do {
-        logger.info { "Enter Item: " }
-        item = readln()
+    do { // show gui
+        do { // error handling
 
-        //error handling
-            do {
-                logger.info { "Quantity: " }
-                quantity = readln()
-                if (quantity.toIntOrNull() == null) {
-                    logger.warn { "Enter a valid quantity" }
+            logger.info { "1. New Customer" }
+            logger.info { "2. Exit Program" }
+            guiAnswer = readln()
+            if (guiAnswer.toIntOrNull() == null) {
+                logger.info { "Enter a valid choice." }
+            }
+        } while (guiAnswer?.toIntOrNull() == null)
+
+            if (guiAnswer.toInt() == 1) {
+                //clear the array lists values
+                itemsBoughtList.clear()
+                itemsQuantity.clear()
+                pricePerItemList.clear()
+                totalPerItemList.clear()
+                numberList.clear()
+                numberID = 0
+                logger.info { "Show PoS" }
+                do {
+                    logger.info { "Enter Item: " }
+                    item = readln()
+
+                    //error handling
+                    do {
+                        logger.info { "Quantity: " }
+                        quantity = readln()
+                        if (quantity.toIntOrNull() == null) {
+                            logger.warn { "Enter a valid quantity" }
+                        }
+                    } while (quantity.toIntOrNull() == null)
+
+                    do {
+                        logger.info { "Price: " }
+                        price = readln()
+                        if (price.toDoubleOrNull() == null) {
+                            logger.warn { "Enter a valid price" }
+                        }
+                    } while (price.toDoubleOrNull() == null)
+
+                    //add User Input to List
+                    itemsBoughtList.add(item)
+                    pricePerItemList.add(price.toDouble())
+                    itemsQuantity.add(quantity.toInt())
+                    quantityTimesPrice = quantity.toDouble() * price.toDouble()
+                    totalPerItemList.add(quantityTimesPrice)
+                    numberID += 1
+                    numberList.add(numberID)
+
+                    //error handling
+                    do {
+                        logger.info { "Adding more items? Y/N: " }
+                        yesNo = readln()
+                    } while (yesNo.uppercase() != "Y" && yesNo.uppercase() != "N")
+
+                } while (yesNo.uppercase() == "Y")
+
+                //log all the items like a receipt would
+                var q = 0
+                for (i in numberList) {
+                    q = i - 1
+                    logger.info { "Item No. $i: ${itemsBoughtList[q].uppercase()} - QTY: ${itemsQuantity[q]} - Price: ${pricePerItemList[q]} - Total: ${totalPerItemList[q]}" }
                 }
-            } while (quantity.toIntOrNull() == null)
-            do {
-                logger.info { "Price: " }
-                price = readln()
-                if (price.toDoubleOrNull() == null) {
-                    logger.warn { "Enter a valid price" }
+
+                //sum of all totalPerItemList list elements
+                var total = 0.0
+                for (i in totalPerItemList) {
+                    total += i
                 }
-            }while (price.toDoubleOrNull() == null)
-
-        //add User Input to List
-        itemsBoughtList.add(item)
-        pricePerItemList.add(price.toDouble())
-        itemsQuantity.add(quantity.toInt())
-        quantityTimesPrice = quantity.toDouble() * price.toDouble()
-        totalPerItemList.add(quantityTimesPrice)
-        numberID += 1
-        numberList.add(numberID)
-        //error handling
-        do {
-            logger.info { "Adding more items? Y/N: " }
-            yesNo = readln()
-        } while (yesNo.uppercase() != "Y" && yesNo.uppercase() != "N")
-
-    } while (yesNo.uppercase() == "Y")
-
-    //log all the items like a receipt would
-    var q = 0
-    for (i in numberList) {
-        q = i-1
-        logger.info { "Item No. $i: ${itemsBoughtList[q].uppercase()} - QTY: ${itemsQuantity[q]} - Price: ${pricePerItemList[q]} - Total: ${totalPerItemList[q]}" }
-    }
-
-    //sum of all totalPerItemList list elements
-    var total = 0.0
-    for (i in totalPerItemList) {
-        total += i
-    }
-    logger.info { "----------" }
-    logger.info { "TOTAL: $total" }
+                logger.info { "----------" }
+                logger.info { "TOTAL: $total" }
+                programExit = false
+            } else if (guiAnswer.toInt() == 2) {
+                logger.info { "Program Exits..." }
+                programExit = true
+            } else {
+                logger.info { "Enter a valid choice." }
+                programExit = false
+            }
+    } while (programExit == false)
 }
