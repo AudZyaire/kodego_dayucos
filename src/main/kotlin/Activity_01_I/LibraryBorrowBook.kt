0ba7d140d1fe94e6f45bb5bdf3043329f20ff5b8
AudZyaire <jjdayucos04@gmail.com>
2022-10-25 13:48:55 +0800

    Modified Activity_01_I - LibraryBorrowBook.kt - added GUI

--- src/main/kotlin/Activity_01_I/LibraryBorrowBook.kt
+++ src/main/kotlin/Activity_01_I/LibraryBorrowBook.kt
@@ -18,48 +18,67 @@ fun main() {
     var dateBorrowed: String
     var dateReturn: String
     var yesNo = "y"
+    var guiAnswer:String? = null
+    var exitProgram = true
 
-    logger.info {"---Library Book Borrowing System---"}
-    do {
-            logger.info {"Enter Student ID: "}
-            var studId = readln()
-            logger.info {"Enter Last Name: "}
-            var lastName = readln()
-            logger.info {"Enter First Name: "}
-            var firstName = readln()
-            logger.info {"Enter Title: "}
-            var title = readln()
-            logger.info {"How long can it be borrowed?"}
-            //loop until daysBorrowed is an Integer
-            do {
-                logger.info {"Enter number of days: "}
-                daysBorrowed = readln()
-            }while (daysBorrowed.toIntOrNull()==null)
+        do { //Show GUI
+            logger.info { "-- Library System --" }
+            logger.info { "1. Book Borrow" }
+            logger.info { "2. Show Books Borrowed/ Borrower information" }
+            logger.info { "3. Exit Program" }
+            guiAnswer = readln()
+            if (guiAnswer.toInt() == 1) {
 
-            //add to arrays
-            studentIDList.add(studId)
-            lastNameList.add(lastName)
-            firstNameList.add(firstName)
-            bookTitleList.add(title)
-            dateBorrowed=dateTimeNow.format(dateFormat1)
-            dateBorrowedList.add(dateBorrowed)
-            dateReturn = dateTimeNow.plusDays(daysBorrowed.toLong()).format(dateFormat1)
-            dateReturnList.add(dateReturn)
-        do {
-            println("Another student wants to borrow? Y/N")
-            yesNo = readln()
-        }while (yesNo.uppercase() != "Y" && yesNo.uppercase() != "N")
+                do {
+                    logger.info {"Enter Student ID: "}
+                    var studId = readln()
+                    logger.info {"Enter Last Name: "}
+                    var lastName = readln()
+                    logger.info {"Enter First Name: "}
+                    var firstName = readln()
+                    logger.info {"Enter Title: "}
+                    var title = readln()
+                    logger.info {"How long can it be borrowed?"}
+                    //loop until daysBorrowed is an Integer
+                    do {
+                        logger.info {"Enter number of days: "}
+                        daysBorrowed = readln()
+                    }while (daysBorrowed.toIntOrNull()==null)
 
+                    //add to arrays
+                    studentIDList.add(studId)
+                    lastNameList.add(lastName)
+                    firstNameList.add(firstName)
+                    bookTitleList.add(title)
+                    dateBorrowed=dateTimeNow.format(dateFormat1)
+                    dateBorrowedList.add(dateBorrowed)
+                    dateReturn = dateTimeNow.plusDays(daysBorrowed.toLong()).format(dateFormat1)
+                    dateReturnList.add(dateReturn)
+                    do {
+                        println("Another student wants to borrow? Y/N")
+                        yesNo = readln()
+                    }while (yesNo.uppercase() != "Y" && yesNo.uppercase() != "N")
 
-    }while (yesNo.uppercase() == "Y")
-    logger.info { "-----------------------" }
-    logger.info { "-- List of borrowers --" }
-    for (i in studentIDList.indices) {
-        logger.info { "Student ID: ${studentIDList[i]}" }
-        logger.info { "Student's Name: ${lastNameList[i]}, ${firstNameList[i]}"}
-        logger.info {"Book Title: ${bookTitleList[i]}"}
-        logger.info {"Date Borrowed: ${dateBorrowedList[i]}"}
-        logger.info {"Date to be Returned: ${dateReturnList[i]} "}
-        logger.info {"-----------------------"}
-    }
+                }while (yesNo.uppercase() == "Y")
+                exitProgram = false
+            } else if (guiAnswer.toInt() == 2) {
+                logger.info { "-----------------------" }
+                logger.info { "-- List of borrowers --" }
+                for (i in studentIDList.indices) {
+                    logger.info { "Student ID: ${studentIDList[i]}" }
+                    logger.info { "Student's Name: ${lastNameList[i]}, ${firstNameList[i]}"}
+                    logger.info {"Book Title: ${bookTitleList[i]}"}
+                    logger.info {"Date Borrowed: ${dateBorrowedList[i]}"}
+                    logger.info {"Date to be Returned: ${dateReturnList[i]} "}
+                    logger.info {"-----------------------"}
+                }
+                exitProgram = false
+            } else if (guiAnswer.toInt() == 3) {
+                logger.info { "Program exits..." }
+                exitProgram = true
+            } else {
+                exitProgram = false
+                logger.warn { "Input Error" }
+            }
+        } while (exitProgram == false)
 }
